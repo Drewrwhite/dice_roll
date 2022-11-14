18c69e15a38a04ffcdaa0fdfdaafdccfb4c15795
Drew White <d.white0002@gmail.com>
2022-11-14 15:49:25 -0800

    add diagram for dice and function to return diagram on result

--- dice.py
+++ dice.py
@@ -73,6 +73,32 @@ def roll_dice(num_dice):
     roll_results.append(roll)
   return roll_results
 
+def generate_dice_faces_diagram(dice_values):
+  """ Return ASCII diagram of dice faces for results
+
+  """
+  # Generate dice faces in list
+  dice_faces = []
+  for value in dice_values:
+    dice_faces.append(DICE_ART[value])
+  # Generate dice face rows in list
+  dice_faces_rows = []
+  for row_idx in range(DIE_HEIGHT):
+    row_components = []
+    for die in dice_faces:
+      row_components.append(die[row_idx])
+    row_string = DIE_FACE_SEPARATOR.join(row_components)
+    dice_faces_rows.append(row_string)
+  # Generate header for RESULTS centered
+  width = len(dice_faces_rows[0])
+  diagram_header = " RESULTS ".center(width, "~")
+
+  dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
+  return dice_faces_diagram
+
+  
+
+
 
 # ~~~ App's main code block ~~~
 # Get and validate user input
@@ -80,6 +106,8 @@ num_dice_input = input("How many dice do you want to roll? [1-6] ")
 num_dice = parse_input(num_dice_input)
 # Roll dice
 roll_results = roll_dice(num_dice)
-
-print(roll_results)
+# Generate ASCII diagram of dice faces
+dice_face_diagram = generate_dice_faces_diagram(roll_results)
+# Display diagram
+print(f"\n{dice_face_diagram}")
 
