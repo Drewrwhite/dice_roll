01f13bedf6c4137c79b5cc0a364b108ec2ec694f
Drew White <d.white0002@gmail.com>
2022-11-14 15:28:51 -0800

    add function to roll defined number of dice and return random nums

--- dice.py
+++ dice.py
@@ -1,9 +1,11 @@
-def parse_input(input_string):
-    """Return `input_string` as an integer between 1 and 6.
+import random
 
-    Check if `input_string` is an integer number between 1 and 6.
-    If so, return an integer with the same value. Otherwise, tell
-    the user to enter a valid number and quit the program.
+def parse_input(input_string):
+    """Return `input_string` as num between 1 and 6.
+
+    Check if `input_string` is num between 1 and 6.
+    If so, return num with the same value. Otherwise, tell  user to enter valid number and quit program.
+
     """
     if input_string.strip() in {"1", "2", "3", "4", "5", "6"}:
         return int(input_string)
@@ -11,9 +13,25 @@ def parse_input(input_string):
         print("Please enter a number from 1 to 6.")
         raise SystemExit(1)
 
+def roll_dice(num_dice):
+  """ Return list of integers with length `num_dice`
+
+  Each num in return list is random num between 1 - 6 
+
+  """
+  roll_results = []
+  for _ in range(num_dice):
+    roll = random.randint(1, 6)
+    roll_results.append(roll)
+  return roll_results
+
 
 # ~~~ App's main code block ~~~
-# 1. Get and validate user's input
+# Get and validate user input
 num_dice_input = input("How many dice do you want to roll? [1-6] ")
 num_dice = parse_input(num_dice_input)
+# Roll dice
+roll_results = roll_dice(num_dice)
+
+print(roll_results)
 
